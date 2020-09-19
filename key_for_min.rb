65f4d470778669a10308cf43c162793d3ed375ea
Nattida Wong <nattidac.wong@gmail.com>
2020-09-19 22:19:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,26 +1,15 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
-# def key_for_min_value(name_hash)
-#   value = 500
-#   key = " "
-#   name_hash.each do |n, x|
-#     if value > x
-#       value = x
-#       key = n
-#       return key
-#     end
-#   end
-#   return nil
-# end
+prereqs: iterators, hashes, conditional logic
+Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = nil
-  hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  value = 500
+  key = " "
+  name_hash.each do |n, x|
+    if value > x
+      value = x
+      key = n
+      return key
     end
   end
-  lowest_key
+  return nil
 end
