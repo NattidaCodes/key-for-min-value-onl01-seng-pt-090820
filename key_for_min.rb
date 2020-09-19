3d45926fddf699fcb2917908167c1ed81df97477
Nattida Wong <nattidac.wong@gmail.com>
2020-09-19 21:36:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
-    if value < lowest
-      return "#{key}"
-    end
-  end
+  name_hash.each do |n|
+    if n == {}
+      return nil
 end
